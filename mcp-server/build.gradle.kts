ffa2e87a5899b229d852f4f66bcd2ae684196f78
Roger Vinas <roger.vinas@fastzink.com>
2025-04-10 19:01:37 +0200

    Add mcp-server

---
+++ mcp-server/build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    val kotlinVersion = "2.1.20"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.rogervinas"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+val springAiVersion = "1.0.0-M6"
+
+dependencies {
+    implementation("org.springframework.ai:spring-ai-mcp-server-webflux-spring-boot-starter")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    testImplementation(platform("org.junit:junit-bom:5.12.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.ai:spring-ai-bom:$springAiVersion")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
