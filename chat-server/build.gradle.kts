ad0d2e95610a906d22a92e2be6a671777f9e146c
Roger Vinas <roger.vinas@fastzink.com>
2025-04-10 19:24:18 +0200

    Add chat-server

---
+++ chat-server/build.gradle.kts
@@ -0,0 +1,61 @@
+plugins {
+    val kotlinVersion = "2.1.20"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    id("org.springframework.boot") version "3.4.4"
+    id("io.spring.dependency-management") version "1.1.7"
+}
+
+group = "com.rogervinas"
+version = "0.0.1-SNAPSHOT"
+
+java {
+    toolchain {
+        languageVersion = JavaLanguageVersion.of(21)
+    }
+}
+
+repositories {
+    mavenCentral()
+}
+
+val springAiVersion = "1.0.0-M6"
+val flywayVersion = "11.6.0"
+
+dependencies {
+    implementation("org.springframework.ai:spring-ai-mcp-client-spring-boot-starter")
+    implementation("org.springframework.ai:spring-ai-bedrock-converse-spring-boot-starter")
+    implementation("org.springframework.ai:spring-ai-bedrock-ai-spring-boot-starter")
+
+    implementation("org.springframework.ai:spring-ai-pgvector-store-spring-boot-starter")
+    runtimeOnly("org.postgresql:postgresql")
+
+    implementation("org.flywaydb:flyway-core:$flywayVersion")
+    implementation("org.flywaydb:flyway-database-postgresql:$flywayVersion")
+
+    implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+
+    testImplementation(platform("org.junit:junit-bom:5.12.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    testImplementation("org.junit.jupiter:junit-jupiter-params")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.ai:spring-ai-bom:$springAiVersion")
+    }
+}
+
+kotlin {
+    compilerOptions {
+        freeCompilerArgs.addAll("-Xjsr305=strict")
+    }
+}
+
+tasks.withType<Test> {
+    useJUnitPlatform()
+}
---
+++ chat-server/src/main/kotlin/com/rogervinas/ChatServerApplication.kt
@@ -0,0 +1,89 @@
+package com.rogervinas
+
+import io.modelcontextprotocol.client.McpClient
+import io.modelcontextprotocol.client.McpSyncClient
+import io.modelcontextprotocol.client.transport.HttpClientSseClientTransport
+import org.springframework.ai.chat.client.ChatClient
+import org.springframework.ai.chat.client.advisor.PromptChatMemoryAdvisor
+import org.springframework.ai.chat.client.advisor.QuestionAnswerAdvisor
+import org.springframework.ai.chat.memory.InMemoryChatMemory
+import org.springframework.ai.mcp.SyncMcpToolCallbackProvider
+import org.springframework.ai.vectorstore.VectorStore
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.annotation.Id
+import org.springframework.data.repository.ListCrudRepository
+import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.ResponseBody
+import java.util.concurrent.ConcurrentHashMap
+
+
+@SpringBootApplication
+class ChatServerApplication
+
+@Configuration
+internal class ConversationalConfiguration {
+    @Bean
+    fun mcpClient(@Value("\${mcp-server.url}") url: String) = McpClient
+        .sync(HttpClientSseClientTransport(url))
+        .build().apply {
+            initialize()
+        }
+
+    @Bean
+    fun chatClient(
+        mcpSyncClient: McpSyncClient,
+        builder: ChatClient.Builder
+    ): ChatClient {
+        val system = """
+                You are an AI powered assistant to help people adopt a dog from the adoption 
+                agency named Pooch Palace with locations in Atlanta, Antwerp, Seoul, Tokyo, Singapore, Paris, 
+                Mumbai, New Delhi, Barcelona, San Francisco, and London. Information about the dogs available 
+                will be presented below. If there is no information, then return a polite response suggesting we 
+                don't have any dogs available.
+                
+                If the response involves a timestamp, be sure to convert it to something human-readable.
+                
+                Do _not_ include any indication of what you're thinking. Nothing should be sent to the client between <thinking> tags. 
+                Just give the answer.
+                
+                """.trimIndent()
+        return builder
+            .defaultSystem(system)
+            .defaultTools(SyncMcpToolCallbackProvider(mcpSyncClient))
+            .build()
+    }
+}
+
+internal interface DogRepository : ListCrudRepository<Dog, Int>
+
+data class Dog(@Id val id: Int, val name: String, val owner: String, val description: String)
+
+@Controller
+@ResponseBody
+internal class ConversationalController(vectorStore: VectorStore, private val chatClient: ChatClient) {
+    private val questionAnswerAdvisor = QuestionAnswerAdvisor(vectorStore)
+    private val chatMemory = ConcurrentHashMap<String, PromptChatMemoryAdvisor>()
+
+    @PostMapping("/{id}/inquire")
+    fun inquire(@PathVariable id: String, @RequestParam question: String): String? {
+        val promptChatMemoryAdvisor = chatMemory
+            .computeIfAbsent(id) { _: String -> PromptChatMemoryAdvisor.builder(InMemoryChatMemory()).build() }
+        return chatClient
+            .prompt()
+            .user(question)
+            .advisors(questionAnswerAdvisor, promptChatMemoryAdvisor)
+            .call()
+            .content()
+    }
+}
+
+fun main(args: Array<String>) {
+    runApplication<ChatServerApplication>(*args)
+}
