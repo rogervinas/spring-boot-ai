93c3f39ab8add92f93967826ae54f8c358136f31
Roger Vinas <roger.vinas@fastzink.com>
2026-05-31 11:18:43 +0200

    Fix conversation ID

--- chat-server/src/main/kotlin/com/rogervinas/chat/ChatService.kt
+++ chat-server/src/main/kotlin/com/rogervinas/chat/ChatService.kt
@@ -5,13 +5,13 @@ import org.springframework.ai.chat.client.ChatClient
 import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor
 import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor
 import org.springframework.ai.chat.client.advisor.vectorstore.QuestionAnswerAdvisor
+import org.springframework.ai.chat.memory.ChatMemory
 import org.springframework.ai.chat.memory.InMemoryChatMemoryRepository
 import org.springframework.ai.chat.memory.MessageWindowChatMemory
 import org.springframework.ai.vectorstore.VectorStore
 import org.springframework.stereotype.Service
 import java.time.Clock
 import java.time.LocalDate
-import java.util.concurrent.ConcurrentHashMap
 
 @Service
 class ChatService(vectorStore: VectorStore, private val clock: Clock, private val chatClient: ChatClient) {
@@ -19,21 +19,19 @@ class ChatService(vectorStore: VectorStore, private val clock: Clock, private va
     private val logger = LoggerFactory.getLogger(ChatService::class.java)
     private val questionAnswerAdvisor = QuestionAnswerAdvisor.builder(vectorStore).build()
     private val simpleLoggerAdvisor = SimpleLoggerAdvisor()
-    private val chatMemory = ConcurrentHashMap<String, MessageChatMemoryAdvisor>()
+    private val chatMemoryAdvisor = MessageChatMemoryAdvisor.builder(
+        MessageWindowChatMemory.builder()
+            .chatMemoryRepository(InMemoryChatMemoryRepository())
+            .build()
+    ).build()
 
     fun chat(chatId: String, question: String): String {
-        val chatMemoryAdvisor = chatMemory.computeIfAbsent(chatId) {
-            MessageChatMemoryAdvisor.builder(
-                MessageWindowChatMemory.builder()
-                    .chatMemoryRepository(InMemoryChatMemoryRepository())
-                    .build()
-            ).build()
-        }
         return chatClient
             .prompt()
             .system { it.param("currentDate", LocalDate.now(clock)) }
             .user(question)
             .advisors(questionAnswerAdvisor, chatMemoryAdvisor, simpleLoggerAdvisor)
+            .advisors { a -> a.param(ChatMemory.CONVERSATION_ID, chatId) }
             .call()
             .content().apply {
                 logger.info("Chat #$chatId question: $question")
